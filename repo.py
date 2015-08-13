02e9c9899c0d10c3d15299f8ff32c46182cad77a
CUI Hao <cuihao.leo@gmail.com>
2015-08-13 13:13:35 +0800

    Use pkg_resources.parse_version to parse version string

--- repo.py
+++ repo.py
@@ -1,8 +1,11 @@
+import re
+from functools import total_ordering
 from collections import namedtuple
 from nvchecker.get_version import get_version
 from helper.pkgbuild import pkgbuild_parser
 from abc import ABCMeta, abstractmethod
 from tornado.platform.asyncio import AsyncIOMainLoop
+from pkg_resources import parse_version
 import asyncio
 
 # Tell Tornado to use the asyncio eventloop
@@ -13,11 +16,12 @@ PackageVersion = namedtuple('PackageVersion',
 
 
 class Pac(metaclass=ABCMeta):
+    VersionFactory = parse_version
 
     @abstractmethod
-    def __init__(self, name, local_version):
+    def __init__(self, name):
         self.name = name
-        self.local_version = local_version
+        self.local_version = None
         self.remote_version = None
         self._info = {}
         self._on_local_update = []
@@ -30,23 +34,32 @@ class Pac(metaclass=ABCMeta):
         else:
             raise AttributeError(attr)
 
-    def update_local_version(self, version=None):
-        if version is None:
-            return
-        else:
-            if version != self.local_version:
-                self.local_version = version
-                self.on_local_update()
+    def update_local_version(self, version):
+        if isinstance(version, str):
+            version = self.VersionFactory(version)
+
+        if self.local_version is None or version != self.local_version:
+            self.local_version = version
+            self.on_local_update()
 
     @asyncio.coroutine
     def async_update_remote_version(self):
         future = asyncio.Future()
 
         def cb(name, newver):
-            if newver is not None and newver != self.remote_version:
+            future.set_result(newver is not None)
+            if newver is None:
+                return
+
+            if self._vpatch:
+                newver = self._vpatch(self, newver)
+
+            if isinstance(newver, str):
+                newver = self.VersionFactory(newver)
+
+            if self.remote_version is None or newver != self.remote_version:
                 self.remote_version = newver
                 self.on_remote_update()
-            future.set_result(newver is not None)
 
         get_version(self.name, self.nvconfig, cb)
         return future
@@ -64,7 +77,7 @@ class Pac(metaclass=ABCMeta):
         elif self._nvconfig:
             self._nvconfig.format(
                 self._info, name=self.name,
-                version=self.local_version)
+                version=str(self.local_version))
         else:
             raise AttributeError("Please set nvconfig first!")
 
@@ -86,29 +99,46 @@ class Pac(metaclass=ABCMeta):
         self._vpatch = patch
 
     @property
-    def patched_local_version(self):
-        if self._vpatch is None:
-            return self.local_version
-        else:
-            self._vpatch(self.local_version)
-
-    @property
     def outdated(self):
-        rver = self._vpatch(self.remote_version)
+        rver = self._vpatch(self, self.remote_version)
         return rver > self.local_version
 
 
+@total_ordering
+class PacmanVersion:
+    regex = re.compile(r"^(?:(\d+):)?([^:-]*)(?:-([^-]+))?$")
+
+    def __init__(self, s):
+        m = self.regex.match(s)
+        if not m:
+            print(s)
+            raise ValueError("Version string cannot be parsed")
+
+        epoch, ver, rel = m.groups()
+        self.epoch = int(epoch) if epoch else 0
+        self.ver = parse_version(ver)
+        self.rel = parse_version(rel if rel is not None else "0")
+
+    def __str__(self):
+        return "{}:{}-{}".format(self.epoch, self.ver, self.rel)
+
+    def __eq__(self, other):
+        return (self.epoch, self.ver, self.rel) == \
+               (other.epoch, other.ver, other.rel)
+
+    def __lt__(self, other):
+        return (self.epoch, self.ver, self.rel) < \
+               (other.epoch, other.ver, other.rel)
+
+
 class PKGBUILDPac(Pac):
+    VersionFactory = PacmanVersion
 
     def __init__(self, path):
         d = pkgbuild_parser(path)
-        version = PackageVersion(
-                    d.get("epoch", 0), d['pkgver'], d.get("pkgrel", 0))
 
         super(PKGBUILDPac, self).__init__(
-            name=d['pkgbase'] if 'pkgbase' in d else d['pkgname'][0],
-            local_version=version,
-        )
+            name=d['pkgbase'] if 'pkgbase' in d else d['pkgname'][0])
 
         self._info["path"] = path
         self._info["sources"] = d.get('source', [])
@@ -117,21 +147,39 @@ class PKGBUILDPac(Pac):
     def update_local_version(self, version=None):
         if version is None:
             d = pkgbuild_parser(self.path)
-            version = PackageVersion(
-                          d.get("epoch", 0), d['pkgver'], d.get("pkgrel", 0))
+
+            epoch = d.get("epoch", 0)
+            pkgver = d["pkgver"].split("-")[0]
+            pkgrel = d.get("pkgrel", "0")
+
+            version = self.VersionFactory("%s:%s-%s" % (epoch, pkgver, pkgrel))
+
         super(PKGBUILDPac, self).update_local_version(version)
+
 
 if __name__ == "__main__":
     import sys
     import os
+
+    def on_up(pac):
+        if pac.remote_version > pac.local_version:
+            print("! " + pac.name)
+        else:
+            print("= " + pac.name)
+
+    def dummy_patch(pac, ver):
+        print("I got ver {} from pac {}.".format(pac.name, ver))
+        return ver
 
     pacs = []
     for directory in os.listdir(sys.argv[1]):
         pkgbuild = os.path.join(sys.argv[1], directory, "PKGBUILD")
         if os.path.isfile(pkgbuild):
             pac = PKGBUILDPac(pkgbuild)
+            pac.apply_version_patch(dummy_patch)
             pac.set_raw_nvconfig({"aur": None})
-            pac.on_remote_update(lambda pac: print(pac.remote_version))
+            pac.update_local_version()
+            pac.on_remote_update(on_up)
             pacs.append(pac)
 
     tasks = []
