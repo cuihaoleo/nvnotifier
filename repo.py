eef4ada405e4624d01c387c7a1d1990d1a4eac38
CUI Hao <cuihao.leo@gmail.com>
2015-08-14 16:24:03 +0800

    Rewrite Pac and PKGBUILDPac class. It's easy to serialize them now.

--- repo.py
+++ repo.py
@@ -1,6 +1,6 @@
 import re
+import os
 from functools import total_ordering
-from collections import namedtuple
 from nvchecker.get_version import get_version
 from helper.pkgbuild import pkgbuild_parser
 from abc import ABCMeta, abstractmethod
@@ -11,19 +11,28 @@ import asyncio
 # Tell Tornado to use the asyncio eventloop
 AsyncIOMainLoop().install()
 
-PackageVersion = namedtuple('PackageVersion',
-                            ['epoch', 'ver', 'rel'])
-
 
 class Pac(metaclass=ABCMeta):
     VersionFactory = parse_version
 
     @abstractmethod
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
+        # variables that are safe to be serialized
         self.name = name
-        self.local_version = None
-        self.remote_version = None
-        self._info = {}
+
+        lv = kwargs.pop("local_version", None)
+        if isinstance(lv, str):
+            lv = self.VersionFactory(lv)
+
+        rv = kwargs.pop("remote_version", None)
+        if isinstance(rv, str):
+            rv = self.VersionFactory(rv)
+
+        self.local_version = lv
+        self.remote_version = rv
+        self._info = kwargs
+
+        # don't serialize these (though nvconfig should be safe)
         self._on_local_update = []
         self._on_remote_update = []
         self._vpatch = None
@@ -35,16 +44,16 @@ class Pac(metaclass=ABCMeta):
         else:
             raise AttributeError(attr)
 
-    def update_local_version(self, version):
+    def update_local_version(self, version, runhook=True):
         if isinstance(version, str):
             version = self.VersionFactory(version)
 
         if self.local_version is None or version != self.local_version:
             self.local_version = version
-            self.on_local_update()
+            runhook and self.on_local_update()
 
     @asyncio.coroutine
-    def async_update_remote_version(self):
+    def async_update_remote_version(self, runhook=True):
         future = asyncio.Future()
 
         def cb(name, newver):
@@ -60,7 +69,7 @@ class Pac(metaclass=ABCMeta):
 
             if self.remote_version is None or newver != self.remote_version:
                 self.remote_version = newver
-                self.on_remote_update()
+                runhook and self.on_remote_update()
 
         get_version(self.name, self.nvconfig, cb)
         return future
@@ -145,32 +154,38 @@ class PacmanVersion:
 class PKGBUILDPac(Pac):
     VersionFactory = PacmanVersion
 
-    def __init__(self, path):
-        d = pkgbuild_parser(path)
+    def __init__(self, path, **kwargs):
+        kwargs["path"] = os.path.abspath(path)
+        if "name" not in kwargs:
+            super(PKGBUILDPac, self).__init__(name="", **kwargs)
+            self.update_pkgbuild_info()
+        else:
+            super(PKGBUILDPac, self).__init__(**kwargs)
 
-        super(PKGBUILDPac, self).__init__(
-            name=d['pkgbase'] if 'pkgbase' in d else d['pkgname'][0])
+    def update_pkgbuild_info(self):
+        stat = os.stat(self.path)
 
-        self._info["path"] = path
-        self._info["sources"] = d.get('source', [])
-        self._info["packages"] = d.get('pkgname', [])
-
-    def update_local_version(self, version=None):
-        if version is None:
+        if stat != self._info.get("stat"):
+            self._info["stat"] = stat
             d = pkgbuild_parser(self.path)
+            self.name = d.get('pkgbase', d['pkgname'][0])
+            self._info["packages"] = tuple(d["pkgname"])
+            self._info["sources"] = tuple(d.get('source', []))
 
             epoch = d.get("epoch", 0)
             pkgver = d["pkgver"].split("-")[0]
             pkgrel = d.get("pkgrel", "0")
-
             version = self.VersionFactory("%s:%s-%s" % (epoch, pkgver, pkgrel))
 
-        super(PKGBUILDPac, self).update_local_version(version)
+            super(PKGBUILDPac, self).update_local_version(version)
+
+    def update_local_version(self, version=None):
+        self.update_pkgbuild_info()
 
 
 if __name__ == "__main__":
     import sys
-    import os
+    from serializer import PickledData
 
     def on_up(pac):
         if pac.remote_version > pac.local_version:
@@ -182,21 +197,38 @@ if __name__ == "__main__":
         print("I got ver {} from pac {}.".format(pac.name, ver))
         return ver
 
-    pacs = []
-    for directory in os.listdir(sys.argv[1]):
-        pkgbuild = os.path.join(sys.argv[1], directory, "PKGBUILD")
-        if os.path.isfile(pkgbuild):
-            pac = PKGBUILDPac(pkgbuild)
+    with PickledData(".db", default={}) as D:
+
+        allset = set()
+        for directory in os.listdir(sys.argv[1]):
+            pkgbuild = os.path.join(sys.argv[1], directory, "PKGBUILD")
+            if os.path.isfile(pkgbuild):
+                allset.add(os.path.abspath(pkgbuild))
+
+        saved = D.get("pacs", [])
+        saved_set = {pac["path"] for pac in saved}
+
+        pacs = []
+        for pacinfo in saved:
+            if pacinfo["path"] in allset:
+                pacs.append(PKGBUILDPac(**pacinfo))
+
+        for path in allset-saved_set:
+            pac = PKGBUILDPac(path)
             pac.apply_version_patch(dummy_patch)
+            pacs.append(pac)
+
+        for pac in pacs:
             pac.set_raw_nvconfig({"aur": None})
             pac.update_local_version()
             pac.on_remote_update(on_up)
-            pacs.append(pac)
 
-    tasks = []
-    for pac in pacs:
-        task = asyncio.async(pac.async_update_remote_version())
-        tasks.append(task)
+        tasks = []
+        for pac in pacs:
+            task = asyncio.async(pac.async_update_remote_version())
+            tasks.append(task)
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait(tasks))
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait(tasks))
+
+        D["pacs"] = [pac.info for pac in pacs]
