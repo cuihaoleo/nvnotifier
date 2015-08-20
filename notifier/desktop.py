83adf48675d65ee5b819a0c95217880f9e244487
CUI Hao <cuihao.leo@gmail.com>
2015-08-20 22:08:04 +0800

    desktop notifier

---
+++ notifier/desktop.py
@@ -0,0 +1,33 @@
+import logging
+import datetime
+import asyncio
+import subprocess
+from .lib import convert_timeout_to_second
+import nvchecker.lib.notify as notify
+
+logger = logging.getLogger(__name__)
+
+
+def send_notify(title, body):
+    subprocess.call(["notify-send", title, body, "--icon=face-sad"])
+
+
+class Notifier:
+    def __init__(self, *args, timeout="0s", **kwargs):
+        timeout_seconds = convert_timeout_to_second(timeout)
+        now = datetime.datetime.now()
+        self.deadline = now - datetime.timedelta(seconds=timeout_seconds)
+
+    def send(self, pac, outtime):
+        if outtime > self.deadline:
+            return False
+
+        send_notify('%s is out-of-date' % pac.name,
+                    'Local: %s\nRemote: %s' % 
+                    (pac.local_version, pac.remote_version))
+        logger.debug("Notify %s via desktop notification" % pac)
+
+        return True
+
+    def finish(self):
+        pass
